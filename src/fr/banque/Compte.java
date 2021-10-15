712d6164c5d4daa3403e614847be0bd4ad0fca66
Elvis Messiaen <elvism72@gmail.com>
2021-10-15 19:08:23 +0200

    créations class Compte

--- src/fr/banque/Compte.java
+++ src/fr/banque/Compte.java
@@ -1,4 +1,48 @@
 package fr.banque;
 
 public class Compte {
+    private int numero;
+    private double solde;
+
+    public Compte() {
+
+    }
+
+    public Compte(int numero, double solde) {
+        this.numero = numero;
+        this.solde = solde;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public double getSolde() {
+        return solde;
+    }
+
+    public void setSolde(double solde) {
+        this.solde = solde;
+    }
+
+    public void ajouter(double unMontant) {
+        this.solde += unMontant;
+    }
+
+    public void retirer(double unMontant) {
+        this.solde -= unMontant;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Compte{" +
+                "numero=" + numero +
+                ", solde=" + solde +
+                '}';
+    }
 }
